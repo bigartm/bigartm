3bfe46caf447c7410b1854b7ff64445644e62813
Murat Apishev <great-mel@yandex.ru>
2014-10-09 23:54:05 +0400

    fix2.1

--- src/artm/core/topic_model.h
+++ src/artm/core/topic_model.h
@@ -78,14 +78,14 @@ class TopicWeightIterator {
   const float* n_w_;
   const float* r_w_;
   const float* n_t_;
-  std::vector<std::string>& topics_name_;
+  const std::vector<std::string>& topics_name_;
   int topics_count_;
   mutable int current_topic_;
 
   TopicWeightIterator(const float* n_w,
                       const float* r_w,
                       const float* n_t,
-                      std::vector<std::string>& topics_name)
+                      const std::vector<std::string>& topics_name)
       : n_w_(n_w),
         r_w_(r_w),
         n_t_(n_t),
