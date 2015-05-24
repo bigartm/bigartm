e6a6a0ac704a56eda43fd917c86428164a0ec269
Murat Apishev <great-mel@yandex.ru>
2015-05-24 20:48:37 +0400

    Add coherency reg and score, modify dictionary

--- src/artm/core/dictionary.h
+++ src/artm/core/dictionary.h
@@ -3,8 +3,9 @@
 #ifndef SRC_ARTM_CORE_DICTIONARY_H_
 #define SRC_ARTM_CORE_DICTIONARY_H_
 
-#include <string>
 #include <map>
+#include <string>
+#include <vector>
 
 #include "artm/messages.pb.h"
 #include "artm/core/common.h"
@@ -13,8 +14,29 @@
 namespace artm {
 namespace core {
 
-typedef std::map<Token, ::artm::DictionaryEntry> DictionaryMap;
-typedef ThreadSafeCollectionHolder<std::string, DictionaryMap> ThreadSafeDictionaryCollection;
+class Dictionary {
+ public:
+  explicit Dictionary(const artm::DictionaryConfig& config);
+
+  int cooc_size(const Token& token) const;
+  const Token* cooc_token(const Token& token, int index) const;
+
+  // cooc_value() methods return 0, if there're no such tokens
+  int cooc_value(const Token& token, int index) const;
+  int cooc_value(const Token& token_1, const Token& token_2) const;
+
+  const DictionaryEntry* entry(const Token& token) const;
+  const DictionaryEntry* entry(int index) const { return &entries_[index]; }
+  inline int size() const { return entries_.size(); }
+
+ private:
+  std::vector<DictionaryEntry> entries_;
+  std::map<Token, int> token_index_;
+  std::map<int, Token> index_token_;
+  std::map<int, std::map<int, int> > cooc_values_;
+};
+
+typedef ThreadSafeCollectionHolder<std::string, Dictionary> ThreadSafeDictionaryCollection;
 
 }  // namespace core
 }  // namespace artm
