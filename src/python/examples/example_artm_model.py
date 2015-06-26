fe75bac1f48d09f8f7c039d0c0c02e1363775a30
Murat Apishev <great-mel@yandex.ru>
2015-06-26 14:15:24 +0300

    Minor fixes

--- src/python/examples/example_artm_model.py
+++ src/python/examples/example_artm_model.py
@@ -1,57 +1,66 @@
-# Tests for new BigARTM Python API.
+# This is a simple example of basic usage of BigARTM's new Python API.
+
+import glob
+
 import artm.artm_model
 from artm.artm_model import *
 
-model = ArtmModel(num_processors=2, document_passes_count=10,
-                  topics_count = 7, class_ids={'@default_class' : 1.0})
+# Create the model objects
+model = ArtmModel(num_processors=2, topics_count = 15, document_passes_count=1)
 
-#model.parse(data_format='bow_uci', collection_name='kos')
+# Parse collection if it is necessary
+if len(glob.glob('kos' + "/*.batch")) < 1:
+    model.parse(data_path='', data_format='bow_uci', collection_name='kos')
+
+# Initialize model using dictionary (e.g. fill create Phi matrix using information about
+# vocabulary and topics count with random values in (0, 1))
 model.initialize(dictionary=model.load_dictionary('kos/dictionary'))
 
-model.scores.add(SparsityPhiScore(name='SpPhiScore'))
-model.scores.add(SparsityThetaScore(name='SpThetaScore'))
-model.scores.add(PerplexityScore(name='PerpScore'))
-model.scores.add(ThetaSnippetScore(name='SnipScore', num_items=2))
-model.scores.add(TopicKernelScore(name='KernelScore', probability_mass_threshold=0.9))
-model.scores.add(ItemsProcessedScore(name='ItemScore'))
+# Create scores for model quality control
+model.scores.add(SparsityPhiScore(name='SparsityPhiScore'))
+model.scores.add(SparsityThetaScore(name='SparsityThetaScore'))
+model.scores.add(PerplexityScore(name='PerplexityScore'))
 
-model.regularizers.add(SmoothSparsePhiRegularizer('SmSpPhiReg', -0.1, ['@default_class']))
-model.regularizers.add(SmoothSparseThetaRegularizer('SmSpThetaReg', -0.4))
-model.regularizers.add(DecorrelatorPhiRegularizer('DecorPhiReg', 100000))
+# Create regularizers for model
+model.regularizers.add(SmoothSparsePhiRegularizer(name='SparsePhi', tau=-0.3))
+model.regularizers.add(SmoothSparseThetaRegularizer(name='SparseTheta', tau=-0.7))
+model.regularizers.add(DecorrelatorPhiRegularizer(name='DecorrelatorPhi', tau=100000.0))
 
-#model.load('artm_model')
-#print model.topic_names
+# Learn model in offline mode (5 passes over whole collection)
+model.fit_offline(data_path='kos', collection_passes_count=5)
+num_phi_updates = 5
 
-model.fit_offline(data_path='kos', collection_passes_count=2)
-model.regularizers['SmSpPhiReg'].tau = -10
-model.scores.add(TopTokensScore(name='TopTokenScore'))
-model.scores['SnipScore'].num_items = 3
-model.fit_offline(data_path='kos', collection_passes_count=2)
+# Check sparsity
+print 'Sparsity Phi:' + str(model.scores_info['SparsityPhiScore'].value[num_phi_updates-1])
+print 'Sparsity Phi:' + str(model.scores_info['SparsityThetaScore'].value[num_phi_updates-1])
 
-model.fit_online(data_path='kos', batches=['066852ad-cf27-46a1-8f7b-c28b8db77ca9.batch',
-                                           'f2fdf7c1-92eb-4cad-9441-554f67190537.batch'])
-print model.scores_info['SpPhiScore'].value
-print '\n'
-print model.scores_info['SpThetaScore'].value
-print '\n'
-print model.scores_info['PerpScore'].value
-print '\n'
-for item in model.scores_info['SnipScore'].snippet:
-  print  item
-  print '\n'
+# Correct tau coefficients for regularizers to increase sparsity
+model.regularizers['SparsePhi'].tau = -0.5
+model.regularizers['SparseTheta'].tau = -0.9
 
-if not model.scores_info['TopTokenScore'].topic_info[0] is None:
-  print '\n'
-  print model.scores_info['TopTokenScore'].topic_info[0]['topic_1'].tokens
-  print model.scores_info['TopTokenScore'].topic_info[0]['topic_1'].weights
-  print model.scores_info['KernelScore'].topic_info[0]['topic_1'].tokens
+# Add new score
+model.scores.add(TopTokensScore(name='TopTokensScore', num_tokens=5))
 
-retval = model.find_theta(data_path='kos', batches=['a657484f-a028-4800-b965-f8ddf1d742f9.batch'])
-retval = model.get_theta(remove_theta=True)
-print retval.document_ids
-print len(retval.document_ids)
-print model.scores_info['ItemScore'].value
-model.save()
-model.to_csv()
-#vis = model.visualize(dictionary_path='kos/dictionary')
-#vis.to_file('lda.html')
+# Continue learning model in offline mode (15 more passes)
+model.fit_offline(data_path='kos', collection_passes_count=15)
+num_phi_updates += 15
+
+# Print all scores
+print 'Per-iter score info:'
+for i in range(num_phi_updates):
+    print 'Iter# ' + str(i),
+    print ', perplexity: %.2f' % model.scores_info['PerplexityScore'].value[i],
+    print ', sparsity Phi: %.3f' % model.scores_info['SparsityPhiScore'].value[i],
+    print ', sparsity Theta: %.3f,' % model.scores_info['SparsityThetaScore'].value[i]
+
+print 'Final top tokens:'
+for topic_name in model.topic_names:
+    print topic_name + ': ',
+    print model.scores_info['TopTokensScore'].topic_info[num_phi_updates-1][topic_name].tokens
+
+# Save final model to model file and to .csv
+model.save(file_name='kos_artm_model')
+model.to_csv(file_name='kos_artm_model.csv')
+
+vis = model.visualize(num_top_tokens=10, dictionary_path='kos/dictionary')
+vis.to_file(filename='vis.html')
