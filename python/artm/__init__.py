1d92a87bca55a56d34c3871e9bb8460dab9489d5
Peter Romov <peter@romov.ru>
2015-07-17 02:58:53 +0300

    python CMake installing fixed

---
+++ python/artm/__init__.py
@@ -0,0 +1,18 @@
+from artm_model import (
+    ArtmModel,
+
+    DecorrelatorPhiRegularizer,
+    ImproveCoherencePhiRegularizer,
+    LableRegularizationPhiRegularizer,
+    SmoothSparsePhiRegularizer,
+    SmoothSparseThetaRegularizer,
+    SpecifiedSparsePhiRegularizer,
+
+    SparsityPhiScore,
+    ItemsProcessedScore,
+    PerplexityScore,
+    SparsityThetaScore,
+    ThetaSnippetScore,
+    TopicKernelScore,
+    TopTokensScore,
+)
