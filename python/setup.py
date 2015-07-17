4991005ace3cbd7fc796d668963e52b9451dfd78
Peter Romov <peter@romov.ru>
2015-07-17 03:16:26 +0300

    installation fixed

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bigartm',
-    #version='${PACKAGE_VERSION}',
+    version='${PACKAGE_VERSION}', # FIXME: fill version variable in CMakeLists
     package_dir={'': '${CMAKE_CURRENT_SOURCE_DIR}'},
     packages=['artm'],
 )
