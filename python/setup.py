a79cb97cbe34eae0ae661c90e55c065de926e4b1
Peter Romov <peter@romov.ru>
2015-07-17 04:38:42 +0300

    travis script cleanup

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='bigartm',
     version='${PACKAGE_VERSION}', # FIXME: fill version variable in CMakeLists
     package_dir={'': '${CMAKE_CURRENT_SOURCE_DIR}'},
     packages=['artm'],
+    install_requires=[
+        'pandas',
+    ],
 )
