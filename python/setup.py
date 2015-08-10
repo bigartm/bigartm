d4dc6b68579c96fc386c51d3de47ad9f8d7db966
Murat Apishev <great-mel@yandex.ru>
2015-08-10 14:19:04 +0400

    init test for wrapper

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='bigartm',
     version='0.0.1',
-    packages=[
-        'artm',
-    ],
+    packages=find_packages(),
     install_requires=[
         'pandas',
         'numpy',
