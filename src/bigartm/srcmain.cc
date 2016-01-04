095eef859448146d0f7052a4024f6ee42e6f9a61
sashafrey <sashafrey@gmail.com>
2016-01-04 21:59:48 +0100

    BigARTM CLI tweaks for online algorithm - initialize nwt with pwt - do not distinguish the very first update

--- src/bigartm/srcmain.cc
+++ src/bigartm/srcmain.cc
@@ -266,6 +266,7 @@ struct artm_options {
   std::vector<std::string> score;
   std::vector<std::string> final_score;
   std::string pwt_model_name;
+  std::string nwt_model_name;
   std::string main_dictionary_name;
 
   // Other options
@@ -277,6 +278,7 @@ struct artm_options {
 
   artm_options() {
     pwt_model_name = "pwt";
+    nwt_model_name = "nwt";
     main_dictionary_name = "main_dictionary";
   }
 
@@ -916,6 +918,7 @@ int execute(const artm_options& options, int argc, char* argv[]) {
   master_config.set_threads(options.threads);
   master_config.set_inner_iterations_count(options.inner_iterations_count);
   master_config.set_pwt_name(options.pwt_model_name);
+  master_config.set_nwt_name(options.nwt_model_name);
 
   for (auto& topic_name : topic_names)
     master_config.add_topic_name(topic_name);
@@ -1036,6 +1039,11 @@ int execute(const artm_options& options, int argc, char* argv[]) {
     initialize_model_args.mutable_topic_name()->CopyFrom(master_config.topic_name());
     initialize_model_args.set_dictionary_name(options.main_dictionary_name);
     master_component->InitializeModel(initialize_model_args);
+
+    if (options.update_every > 0) {
+      initialize_model_args.set_model_name(options.nwt_model_name);
+      master_component->InitializeModel(initialize_model_args);
+    }
   }
 
   if (options.isModelRequired()) {
@@ -1069,7 +1077,7 @@ int execute(const artm_options& options, int argc, char* argv[]) {
         update_count++;
         update_after += options.update_every;
         fit_online_args.add_update_after(std::min<int>(update_after, batch_file_names.size()));
-        fit_online_args.add_apply_weight((update_count == 1) ? 1.0 : pow(options.tau0 + update_count, -options.kappa));
+        fit_online_args.add_apply_weight(pow(options.tau0 + update_count, -options.kappa));
       } while (update_after < batch_file_names.size());
 
       for (auto& batch_file_name : batch_file_names)
