60846610c1986956e26d9c36cf23a2cb87cafd01
Nikita Shapovalov <shapovalovnikita@list.ru>
2016-03-07 18:54:13 +0300

    Enable making documentation locally with ReadTheDocs theme.

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    # otherwise, readthedocs.org uses their theme by default, so no need to specify it
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
