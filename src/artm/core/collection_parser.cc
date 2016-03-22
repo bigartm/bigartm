5b43abbe5beb3f3989b21892af7edef98a045235
sashafrey <sashafrey@gmail.com>
2016-03-22 22:42:43 +0100

    Do not generate empty class_id when parsing Vowpal wabbit format

--- src/artm/core/collection_parser.cc
+++ src/artm/core/collection_parser.cc
@@ -372,7 +372,7 @@ void CollectionParser::ParseVowpalWabbit() {
     ClassId class_id = DefaultClass;
     for (unsigned elem_index = 1; elem_index < strs.size(); ++elem_index) {
       std::string elem = strs[elem_index];
-      if (elem.size() == 0)
+      if (elem.size() <= 1)
         continue;
       if (elem[0] == '|') {
         class_id = elem.substr(1);
