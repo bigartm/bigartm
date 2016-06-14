57f4bee6e7050e2e8837b82d16e47bf2ee7f8a3c
Nikita Shapovalov <shapovalovnikita@list.ru>
2016-06-14 21:26:37 +0300

    Get new setup.py script to integrate with PyPI

---
+++ setup.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from setuptools import setup, find_packages
+from distutils.spawn import find_executable
+
+DISTUTILS_DEBUG = True
+
+# parse arguments
+import sys
+import os.path
+import tempfile
+import shutil
+import subprocess
+
+# specify classifiers
+BIGARTM_CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Software Development'
+]
+
+
+from distutils.command.build import build as _build
+
+
+# Initialize necessary variables
+# guess name of cmake executable
+# FIXME: make cross-platform guessing
+cmake_exec = "cmake"
+# find absolute path of working directory
+try:
+    filename = __file__
+except NameError:
+    filename = sys.argv[0]
+filename = os.path.abspath(filename)
+if os.path.dirname(filename):
+    os.chdir(os.path.dirname(filename))
+working_dir = os.path.abspath(os.getcwd())
+
+
+class build(_build):
+    def run(self):
+        # Create build directories and run cmake
+        try:
+            build_directory = tempfile.mkdtemp(dir="./")
+            os.chdir(build_directory)
+            # run cmake
+            cmake_process = [cmake_exec]
+            cmake_process.append("../")
+            cmake_process.append("-DBUILD_TESTS=OFF")
+            cmake_process.append("-DBUILD_BIGARTM_CLI=OFF")
+            cmake_process.append("-DBUILD_INTERNAL_PYTHON_API=OFF")
+            # FIXME
+            # validate return code
+            retval = subprocess.call(cmake_process)
+            # run make command
+            make_process = ["make"]
+            make_process.append("-j6")
+            retval = subprocess.call(make_process)
+        finally:
+            os.chdir(working_dir)
+            if os.path.exists(build_directory):
+                shutil.rmtree(build_directory)
+        # _build is an old-style class, so super() doesn't work.
+        _build.run(self)
+
+
+setup(
+    # some common information
+    name='bigartm',
+    version='0.8.1rc1',
+    packages=['artm', 'artm.wrapper'],
+    package_dir={'': './python'},
+
+    # information about dependencies
+    install_requires=[
+        'pandas',
+        'numpy'
+    ],
+    # this option must solve problem with installing
+    # numpy as dependency during `setup.py install` execution
+    # some explanations here:
+    # https://github.com/nengo/nengo/issues/508#issuecomment-64962892
+    # https://github.com/numpy/numpy/issues/2434#issuecomment-65252402
+    setup_requires=[
+        'numpy'
+    ],
+    cmdclass={'build': build},
+
+    # metadata for upload to PyPI
+    license='New BSD license',
+    url='https://github.com/bigartm/bigartm',
+    description='BigARTM: the state-of-the-art platform for topic modeling',
+    classifiers=BIGARTM_CLASSIFIERS,
+    # Who should referred as author and how?
+    # author = 'Somebody'
+    # author_email = 'Somebody\'s email'
+    # Now include `artm_dev` Google group as primary maintainer
+    maintainer='ARTM developers group',
+    maintainer_email='artm_dev+pypi_develop@googlegroups.com'
+)
