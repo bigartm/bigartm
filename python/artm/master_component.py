47f3de19adc3b97f78d28f02e34568fd24ee5206
Oleksandr Frei <oleksandr.frei@gmail.com>
2018-02-28 00:12:57 +0100

    Support for hierarchical models in ARTM class

--- python/artm/master_component.py
+++ python/artm/master_component.py
@@ -147,7 +147,7 @@ def _score_data_func(score_data_type):
 
 def _prepare_config(topic_names=None, class_ids=None, scores=None, regularizers=None, num_processors=None,
                     pwt_name=None, nwt_name=None, num_document_passes=None, reuse_theta=None, cache_theta=None,
-                    args=None):
+                    parent_model_id=None, parent_model_weight=None, args=None):
         master_config = messages.MasterModelConfig()
 
         if args is not None:
@@ -193,6 +193,13 @@ def _prepare_config(topic_names=None, class_ids=None, scores=None, regularizers=
         if cache_theta is not None:
             master_config.cache_theta = cache_theta
 
+        if parent_model_id is not None:
+            master_config.parent_master_model_id = parent_model_id
+            master_config.opt_for_avx = False
+
+        if parent_model_weight is not None:
+            master_config.parent_master_model_weight = parent_model_weight
+
         if pwt_name is not None:
             master_config.pwt_name = pwt_name
 
@@ -208,7 +215,8 @@ def _prepare_config(topic_names=None, class_ids=None, scores=None, regularizers=
 class MasterComponent(object):
     def __init__(self, library=None, topic_names=None, class_ids=None, scores=None, regularizers=None,
                  num_processors=None, pwt_name=None, nwt_name=None, num_document_passes=None,
-                 reuse_theta=None, cache_theta=False, config=None, master_id=None):
+                 reuse_theta=None, cache_theta=False, parent_model_id=None, parent_model_weight=None,
+                 config=None, master_id=None):
         """
 
         :param library: an instance of LibArtm
@@ -224,6 +232,9 @@ class MasterComponent(object):
         :param in num_document_passes: num passes through each document
         :param bool reuse_theta: reuse Theta from previous iteration or not
         :param bool cache_theta: save or not the Theta matrix
+        :param int parent_model_id: master_id of parent model (previous level of hierarchy)
+        :param float parent_model_weight: weight of parent model (plays role in fit_offline;
+                                          defines how much to respect parent model as compared to batches)
         """
         self._lib = library
 
@@ -237,6 +248,8 @@ class MasterComponent(object):
                                         num_document_passes=num_document_passes,
                                         reuse_theta=reuse_theta,
                                         cache_theta=cache_theta,
+                                        parent_model_id=parent_model_id,
+                                        parent_model_weight=parent_model_weight,
                                         args=config)
 
         self._config = master_config
@@ -249,7 +262,7 @@ class MasterComponent(object):
 
     def reconfigure(self, topic_names=None, class_ids=None, scores=None, regularizers=None,
                     num_processors=None, pwt_name=None, nwt_name=None, num_document_passes=None,
-                    reuse_theta=None, cache_theta=None):
+                    reuse_theta=None, cache_theta=None, parent_model_id=None, parent_model_weight=None):
         master_config = _prepare_config(topic_names=topic_names,
                                         class_ids=class_ids,
                                         scores=scores,
@@ -260,6 +273,8 @@ class MasterComponent(object):
                                         num_document_passes=num_document_passes,
                                         reuse_theta=reuse_theta,
                                         cache_theta=cache_theta,
+                                        parent_model_id=parent_model_id,
+                                        parent_model_weight=parent_model_weight,
                                         args=self._config)
 
         self._config = master_config
