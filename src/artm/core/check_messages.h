fbb1470ee1c2824255379c93458e96595248465c
Oleksandr Frei <oleksandr.frei@gmail.com>
2018-02-28 11:42:22 +0100

    Fix bug in hartm (seed must be integer), and add a test

--- src/artm/core/check_messages.h
+++ src/artm/core/check_messages.h
@@ -834,6 +834,7 @@ inline std::string DescribeMessage(const ::artm::InitializeModelArgs& message) {
     ss << ", dictionary_name=" << message.dictionary_name();
   }
   ss << ", topic_name_size=" << message.topic_name_size();
+  ss << ", seed=" << message.seed();
   return ss.str();
 }
 
