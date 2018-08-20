6b16cf04303e00dd38742fac95352be2ab4cbb30
Petr Ostroukhov <ostroukhov@phystech.edu>
2018-08-20 23:46:30 +0300

    sphinx.ext.pngmath (deprecated) -> sphinx.ext.imgmath

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../python'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
     'sphinx.ext.autodoc'
 ]
 
