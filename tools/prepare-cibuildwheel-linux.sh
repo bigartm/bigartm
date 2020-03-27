0256826c9b4aa80449317990339586442b25b853
Victor Bulatov <bt.uytya@gmail.com>
2020-03-28 01:26:56 +0300

    Fleogan Mills, Conflux

--- tools/prepare-cibuildwheel-linux.sh
+++ tools/prepare-cibuildwheel-linux.sh
@@ -16,11 +16,13 @@ curl -L http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.ta
 # manylinux image came with pre-installed cmake 2.8.11.2, while protobuf-3 requires cmake 2.8.12.
 # So, we have to manually install a newer version of cmake.
 # Instructions taken from https://askubuntu.com/questions/355565/how-to-install-latest-cmake-version-in-linux-ubuntu-from-command-line.
+
+if [ -d ~/temp_cmake ]; then rm -rf ~/temp_cmake fi
 mkdir ~/temp_cmake
 cd ~/temp_cmake
 curl -L https://cmake.org/files/v3.9/cmake-3.9.1.tar.gz -o cmake-3.9.1.tar.gz && tar -xzf cmake-3.9.1.tar.gz && cd cmake-3.9.1/ 
 
-./bootstrap.sh > /dev/null
+./bootstrap > /dev/null
 make
 make install
 cd ~ && rm -rf ~/temp_cmake
