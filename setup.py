2c6f723e0bae02c9679d09bd30b131e3da8c6528
Victor Bulatov <bt.uytya@gmail.com>
2020-05-25 20:02:03 +0300

    ok, that was stupid

--- setup.py
+++ setup.py
@@ -133,12 +133,8 @@ class BinaryDistribution(Distribution):
         return False
 
 
-# long-description-content-type does not work for some mystical reason
-# 
-# with open(os.path.join(src_abspath, 'README.md'), encoding='utf-8') as f:
-#     long_description = f.read()
-
-long_description = "See the GitHub repository for details"
+with open(os.path.join(src_abspath, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     package_data={'artm.wrapper': [path_to_lib]},
