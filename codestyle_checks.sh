36ef28ec3b93f8caf28bc92d52f374219d948cda
Victor Bulatov <bt.uytya@gmail.com>
2020-11-09 02:33:38 +0300

    [TEST] disabling codestyle check for now (see #1035)

--- codestyle_checks.sh
+++ codestyle_checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running C++ code checks"
-cat utils/cpplint_files.txt | xargs python utils/cpplint.py --linelength=120 || exit 1
+cat utils/cpplint_files.txt | xargs python -m cpplint --linelength=120 || exit 1
 
 echo "Running Python code checks"
 for scr in python/artm/{artm_model,lda_model,hierarchy_utils,dictionary,batches_utils,regularizers,scores,score_tracker,master_component}.py
